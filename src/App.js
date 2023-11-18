221ffd01bca61e7ac76a30a1dde7f0dcb6aaf6f1
ceun2 <toceun2@gmail.com>
2023-11-18 19:04:05 +0900

    making header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './components/Header';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>Testing</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div>
+      <h1>행복일기장</h1>
+    </div>
+  );
+}
