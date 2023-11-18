df5123328c5c69012f55373aad68d9e3246507d5
ceun2 <toceun2@gmail.com>
2023-11-19 00:57:45 +0900

    trying calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './components/Header';
 import Header from './components/Header';
+import Calendar from './components/Calendar';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Calendar></Calendar>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <div>
-      <h1>행복일기장</h1>
+    <div id="header">
+      <h1>행 복 일 기 장</h1>
     </div>
   );
 }
