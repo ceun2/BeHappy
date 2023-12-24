a38e908c3ce01d11e53dc9b828de297db3f7c3cc
ceun2 <toceun2@gmail.com>
2023-12-24 15:51:29 +0900

    left outline

--- src/components/History.js
+++ src/components/History.js
@@ -1,10 +1,14 @@
 import './History.css';
+import React, { useState } from 'react';
 
-const History = () => {
+const History = (props) => {
+  const [percent, setPercent] = useState('98');
+  const [content, setContent] = useState('어쩌구저쩌구');
+
   return (
     <div id="history">
-      <div id="percent"></div>
-      <div id="content"></div>
+      <div id="percent">행복지수 : {percent}%</div>
+      <div id="content">{content}</div>
     </div>
   );
 };
