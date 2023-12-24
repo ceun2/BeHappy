e00d95a0c2a9061a48abc316da2ccdadd1bbde62
chaeyeon03 <parkcy0815@naver.com>
2023-12-24 19:05:52 +0900

    event 설정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,25 @@ import Header from './components/Header';
 import History from './components/History';
 import Calendar from './components/Calendar';
 
+import React, { useState, useRef } from 'react';
+
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  //새로운 일기 추가하는 함수
+  const onCreate = (date, rating, content) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      id: dataId.current,
+      created_date,
+      rating,
+      content,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
   return (
     <div>
       <Header></Header>
@@ -14,7 +32,7 @@ function App() {
           <Calendar></Calendar>
           <History></History>
         </div>
-        <Diary></Diary>
+        <Diary onCreate={onCreate} />
       </div>
     </div>
   );
--- src/components/Diary.js
+++ src/components/Diary.js
@@ -1,16 +1,66 @@
+import { useState, useRef } from 'react';
 import './Diary.css';
 
-const Diary = () => {
+const Diary = ({ onCreate }) => {
+  //기록하기 버튼을 눌렀을 때 데이터의 아이템을 추가하는 역할
+  const dateInput = useRef();
+  const ratingInput = useRef();
+  const contentInput = useRef();
+
+  const [state, setState] = useState({
+    date: '',
+    rating: 1,
+    content: '',
+  });
+
+  //폼 입력값 처리
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    onCreate(state.date, state.rating, state.content);
+    alert('기록완료!');
+    //글 초기화
+    setState({
+      date: '',
+      rating: 1,
+      content: '',
+    });
+  };
   return (
     <div id="diary">
       <form>
-        <input type="date" />
+        <input
+          type="date"
+          ref={dateInput}
+          name="date"
+          value={state.date}
+          onChange={handleChangeState}
+        />
         <div style={{ marginTop: '20px' }}>
           행복 지수를 퍼센트로 입력하세요!
         </div>
-        <input id="rating"></input>
-        <textarea id="write"></textarea>
-        <button type="submit">기록하기</button>
+        <input
+          id="rating"
+          ref={ratingInput}
+          name="rating"
+          value={state.rating}
+          onChange={handleChangeState}
+        />
+        <textarea
+          id="write"
+          ref={contentInput}
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          기록하기
+        </button>
       </form>
     </div>
   );
